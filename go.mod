5b30f009382566a14585c6553ecee5c94f0dc7a0
Joel Adamson <Drazmataz@gmail.com>
2023-07-06 22:34:43 -0700

    Added a bunch of buttons and fixed some state management

--- go.mod
+++ go.mod
@@ -4,33 +4,38 @@ go 1.18
 
 require (
 	github.com/AllenDang/giu v0.6.2
-	github.com/go-vgo/robotgo v0.100.10
-	golang.design/x/hotkey v0.3.0
+	github.com/go-vgo/robotgo v1.0.0-rc1
+	golang.design/x/hotkey v0.4.1
 )
 
 require (
 	github.com/AllenDang/go-findfont v0.0.0-20200702051237-9f180485aeb8 // indirect
 	github.com/AllenDang/imgui-go v1.12.1-0.20220322114136-499bbf6a42ad // indirect
-	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
+	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // indirect
+	github.com/lufia/plan9stats v0.0.0-20230326075908-cb1d2100619a // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
-	github.com/robotn/gohook v0.31.3 // indirect
+	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
 	github.com/robotn/xgb v0.0.0-20190912153532-2cb92d044934 // indirect
 	github.com/robotn/xgbutil v0.0.0-20190912154524-c861d6f87770 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
-	github.com/shirou/gopsutil v3.21.10+incompatible // indirect
-	github.com/tklauser/go-sysconf v0.3.9 // indirect
-	github.com/tklauser/numcpus v0.3.0 // indirect
-	github.com/vcaesar/gops v0.21.3 // indirect
-	github.com/vcaesar/imgo v0.30.0 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.5 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/vcaesar/gops v0.30.1 // indirect
+	github.com/vcaesar/imgo v0.30.2 // indirect
 	github.com/vcaesar/keycode v0.10.0 // indirect
 	github.com/vcaesar/tt v0.20.0 // indirect
-	golang.org/x/image v0.0.0-20220302094943-723b81ca9867 // indirect
-	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/image v0.8.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 	gopkg.in/eapache/queue.v1 v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,24 +5,52 @@ import (
 	g "github.com/AllenDang/giu"
 	"github.com/go-vgo/robotgo"
 	"golang.design/x/hotkey"
+	"log"
 	"time"
 )
 
 var (
+	version                    = "Version 1.0.5"
 	clickingPeriodInput  int32 = 1000
 	clickingPeriod       int32 = 1000
 	isAutoclickerRunning       = false
+	isLeftClicking             = true
+	isRightClicking            = false
+	isSpacePressing            = false
+	isCtrlPressing             = false
+	isFPressing                = false
+	isEPressing                = false
+	isCPressing                = false
+	isAltTabPressing           = false
 	stopClicking               = make(chan bool)
+	inputTask                  = make(chan inputParameters, 10)
+	isInputTaskRunning         = false
 )
+
+type inputParameters struct {
+	isLeftClicking   bool
+	isRightClicking  bool
+	isSpacePressing  bool
+	isCtrlPressing   bool
+	isFPressing      bool
+	isEPressing      bool
+	isCPressing      bool
+	isAltTabPressing bool
+}
 
 func setClickPeriod() {
 	clickingPeriod = clickingPeriodInput
 	g.Update()
 }
 
+func runningCheckbox() {
+	isAutoclickerRunning = !isAutoclickerRunning
+	return
+}
+
 func loop() {
 	g.SingleWindow().Layout(
-		g.Label("Hmm.."),
+		g.Label(version),
 		g.Row(
 			g.InputInt(&clickingPeriodInput).Label("Clicking period: ").Size(60),
 			g.Button("Set click time.").OnClick(setClickPeriod),
@@ -30,44 +58,127 @@ func loop() {
 		g.Row(
 			g.Label(fmt.Sprintf("%vms between clicks.", clickingPeriod)),
 		),
-		g.Checkbox("Press Control+Shift+S to enable or disable clicking!", &isAutoclickerRunning),
+		g.Checkbox("Press Control+Shift+S to enable or disable clicking!", &isInputTaskRunning).OnChange(runningCheckbox),
+		g.Checkbox("Left click!", &isLeftClicking),
+		g.Checkbox("Right click, too!!", &isRightClicking),
+		g.Checkbox("Click spacebar!", &isSpacePressing),
+		g.Checkbox("Click F!", &isFPressing),
+		g.Checkbox("Click E!", &isEPressing),
+		g.Checkbox("Click C!", &isCPressing),
+		g.Checkbox("Click Ctrl!", &isCtrlPressing),
+		g.Label("If you're clicking slower than 250:"),
+		g.Checkbox("Alt Tab!", &isAltTabPressing),
 	)
 }
 
 func registerHotkey() {
 	hk := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.KeyS)
 	if err := hk.Register(); err != nil {
-		panic("Hotkey registration failed.")
+		log.Fatalf("Hotkey: Failed to register hotkey: %v", err)
 	}
 
 	for range hk.Keydown() {
 		fmt.Println("Hotkey was pressed!")
 		isAutoclickerRunning = !isAutoclickerRunning
 		if isAutoclickerRunning {
-			go clickLeftMouseWithPeriod()
+			go clickStuffWithPeriod()
 		} else {
+			isInputTaskRunning = false
 			stopClicking <- isAutoclickerRunning
 		}
 		g.Update()
 	}
 }
 
-func clickLeftMouseWithPeriod() {
+func clickStuffWithPeriod() {
+	if !isInputTaskRunning {
+		isInputTaskRunning = true
+		go inputLoop()
+	}
+	ticker := time.NewTicker(time.Duration(clickingPeriod) * time.Millisecond)
 	for {
+		if !isAutoclickerRunning {
+			return
+		}
 		select {
 		case <-stopClicking:
 			fmt.Println("Stopped!")
 			return
-		default:
-			go robotgo.Click()
-			time.Sleep(time.Duration(clickingPeriod) * time.Millisecond)
+		case <-ticker.C:
+			fmt.Println("Adding to queue...")
+			task := inputParameters{
+				isLeftClicking:   isLeftClicking,
+				isRightClicking:  isRightClicking,
+				isSpacePressing:  isSpacePressing,
+				isCtrlPressing:   isCtrlPressing,
+				isFPressing:      isFPressing,
+				isEPressing:      isEPressing,
+				isCPressing:      isCPressing,
+				isAltTabPressing: isAltTabPressing,
+			}
+			inputTask <- task
 		}
 	}
 
 }
 
+func inputLoop() {
+	isInputTaskRunning = true
+	for {
+		if !isInputTaskRunning {
+			fmt.Println("Input task not running.")
+			return
+		}
+		select {
+		case <-stopClicking:
+			fmt.Println("Stopped the input loop.")
+			isInputTaskRunning = false
+			return
+		case inputs := <-inputTask:
+			go performInputFunctions(inputs)
+		}
+	}
+}
+
+func performInputFunctions(input inputParameters) {
+	if input.isLeftClicking {
+		robotgo.Click()
+	}
+	if isRightClicking {
+		robotgo.Click("right")
+	}
+	if isFPressing {
+		robotgo.KeyTap("f")
+	}
+	if isEPressing {
+		robotgo.KeyTap("e")
+	}
+	if isCPressing {
+		robotgo.KeyTap("c")
+	}
+	if isSpacePressing {
+		robotgo.KeyTap("space")
+	}
+	if isCtrlPressing {
+		robotgo.KeyDown("ctrl")
+		time.Sleep(25 * time.Millisecond)
+		robotgo.KeyUp("ctrl")
+	}
+	if isAltTabPressing && clickingPeriod > 250 {
+		time.Sleep(1 * time.Millisecond)
+		robotgo.KeyTap("tab", "alt")
+	}
+}
+
+func populateKeys() {
+	var keyStrings []string
+	for key, _ := range robotgo.Keycode {
+		keyStrings = append(keyStrings, key)
+	}
+}
+
 func main() {
-	wnd := g.NewMasterWindow("Go Gadget Autoclicker", 400, 200, 0)
+	wnd := g.NewMasterWindow("Go Gadget Autoclicker", 400, 300, 0)
 
 	go registerHotkey()
 
